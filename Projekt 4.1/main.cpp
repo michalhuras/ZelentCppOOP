5830fce42cd4aeb2f5ab7535952e89d3edf9bb88
Michal huras <michal.huras94@gmail.com>
2017-10-10 15:22:40 +0200

    10.10.2017

--- Projekt 4.1/main.cpp
+++ Projekt 4.1/main.cpp
@@ -22,7 +22,7 @@ int main()
     Punkt pkt1("A",3,17);
     //pkt1.wczytaj();
 
-    Prostokat p1("Prostokat",0,0,6,4);
+    Prostokat p1("Prostokat ",0,0,6,4);
     //p1.wczytaj();
 
     sedzia(pkt1,p1);
